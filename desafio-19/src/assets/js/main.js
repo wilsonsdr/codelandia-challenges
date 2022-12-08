096680332366334f8d89043f43d3f8eeda801494
Wilson Reis <wilsonsdr@gmail.com>
2022-12-07 22:40:57 -0300

    add: intro section

--- desafio-19/src/assets/js/main.js
+++ desafio-19/src/assets/js/main.js
@@ -1,7 +1,11 @@
 const navbar = document.querySelector(".navbar");
 const burguer = document.querySelector(".burguer");
-
 burguer.addEventListener("click", () => {
   burguer.classList.toggle("active");
   navbar.classList.toggle("active");
+
+  navbar.addEventListener("click", () => {
+    burguer.classList.remove("active");
+    navbar.classList.remove("active");
+  });
 });
